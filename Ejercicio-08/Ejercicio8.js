84348dd489f439975534b699ef1eb36b8a40f3a1
UO270176 <UO270176@uniovi.es>
2020-11-26 00:02:57 +0100

    Ejercicio8 fin.

--- Ejercicio-08/Ejercicio8.js
+++ Ejercicio-08/Ejercicio8.js
@@ -18,41 +18,51 @@ class Meteorologia {
             method: 'GET',
             success: function (data) {
                 meteo.datos = data;
-                meteo.verJSON();
-                meteo.verDatos();
+                meteo.cargarJSON();
             },
             error: function () {
                 document.write(this.error);
             }
         });
     }
-    verJSON = function () {
-        document.write("<h2>Datos en JSON desde <a href='http://openweathermap.org'>OpenWeatherMap</a></h2>");
-        document.write("<h2>JSON</h2>")
-        var str = JSON.stringify(this.datos, null, 2);
-        document.write("<pre>" + str + "</pre>");
-    }
-    verDatos = function () {
-        document.write("<h2>Datos</h2>")
-        document.write("<p>" + meteo.datos.weather.icon + "</p>");
-        document.write("<p>Ciudad: " + meteo.datos.name + "</p>");
-        document.write("<p>País: " + meteo.datos.sys.country + "</p>");
-        document.write("<p>Latitud: " + meteo.datos.coord.lat + " grados</p>");
-        document.write("<p>Longitud: " + meteo.datos.coord.lon + " grados</p>");
-        document.write("<p>Temperatura: " + meteo.datos.main.temp + " grados Celsius</p>");
-        document.write("<p>Temperatura máxima: " + meteo.datos.main.temp_max + " grados Celsius</p>");
-        document.write("<p>Temperatura mínima: " + meteo.datos.main.temp_min + " grados Celsius</p>");
-        document.write("<p>Presión: " + meteo.datos.main.pressure + " milímetros</p>");
-        document.write("<p>Humedad: " + meteo.datos.main.humidity + "%</p>");
-        document.write("<p>Amanece a las: " + new Date(meteo.datos.sys.sunrise * 1000).toLocaleTimeString() + "</p>");
-        document.write("<p>Oscurece a las: " + new Date(meteo.datos.sys.sunset * 1000).toLocaleTimeString() + "</p>");
-        document.write("<p>Dirección del viento: " + meteo.datos.wind.deg + "  grados</p>");
-        document.write("<p>Velocidad del viento: " + meteo.datos.wind.speed + " metros/segundo</p>");
-        document.write("<p>Hora de la medida: " + new Date(meteo.datos.dt * 1000).toLocaleTimeString() + "</p>");
-        document.write("<p>Fecha de la medida: " + new Date(meteo.datos.dt * 1000).toLocaleDateString() + "</p>");
-        document.write("<p>Descripción: " + meteo.datos.weather[0].description + "</p>");
-        document.write("<p>Visibilidad: " + meteo.datos.visibility + " metros</p>");
-        document.write("<p>Nubosidad: " + meteo.datos.clouds.all + " %</p>");
+
+    cargarJSON() {
+        $.ajax({
+            dataType: "json",
+            url: this.url,
+            method: 'GET',
+            success: function (datos) {
+                $("pre").text(JSON.stringify(datos, null, 2));
+                var stringDatos = "<div id=\"icono\"><img src='https://openweathermap.org/img/w/" + datos.weather[0].icon + ".png'></img></div>"
+                stringDatos += "<h2>" + datos.name + "</h2>";
+                stringDatos += "<ul><li>País: " + datos.sys.country + "</li>";
+                stringDatos += "<li>Latitud: " + datos.coord.lat + " grados</li>";
+                stringDatos += "<li>Longitud: " + datos.coord.lon + " grados</li>";
+                stringDatos += "<li>Temperatura: " + datos.main.temp + " grados Celsius</li>";
+                stringDatos += "<li>Temperatura máxima: " + datos.main.temp_max + " grados Celsius</li>";
+                stringDatos += "<li>Temperatura mínima: " + datos.main.temp_min + " grados Celsius</li>";
+                stringDatos += "<li>Presión: " + datos.main.pressure + " milibares</li>";
+                stringDatos += "<li>Humedad: " + datos.main.humidity + " %</li>";
+                stringDatos += "<li>Amanece a las: " + new Date(datos.sys.sunrise * 1000).toLocaleTimeString() + "</li>";
+                stringDatos += "<li>Oscurece a las: " + new Date(datos.sys.sunset * 1000).toLocaleTimeString() + "</li>";
+                stringDatos += "<li>Dirección del viento: " + datos.wind.deg + " grados</li>";
+                stringDatos += "<li>Velocidad del viento: " + datos.wind.speed + " metros/segundo</li>";
+                stringDatos += "<li>Hora de la medida: " + new Date(datos.dt * 1000).toLocaleTimeString() + "</li>";
+                stringDatos += "<li>Fecha de la medida: " + new Date(datos.dt * 1000).toLocaleDateString() + "</li>";
+                stringDatos += "<li>Descripción: " + datos.weather[0].description + "</li>";
+                stringDatos += "<li>Visibilidad: " + datos.visibility + " metros</li>";
+                stringDatos += "<li>Nubosidad: " + datos.clouds.all + " %</li></ul>";
+
+                $("p").html(stringDatos);
+            },
+            error: function () {
+                $("h3").html("¡Tenemos problemas! No puedo obtener JSON de <a href='http://openweathermap.org'>OpenWeatherMap</a>");
+                $("h4").remove();
+                $("pre").remove();
+                $("p").remove();
+            }
+        });
     }
 }
+
 var meteo = new Meteorologia();
